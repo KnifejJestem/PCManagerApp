574494256d8ab6f27b361327246827f2a284f866
Mateusz Różyński <knifejjestem@gmail.com>
2026-03-04 23:53:29 +0100

    Linux support and fixes + (not working) benchmark for windows
    Idk whats new here. Its the newest version of the app i got and it works with linux. and also benchmark menu that is not working. other than that app is almost ready i would say for the basic stuff.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import flet as ft
-import websockets
 import asyncio
 import json
 
+import flet as ft
+import websockets
+
 stat_box_width = 620
+
 
 async def main(page: ft.Page):
     ft.Theme.use_material3 = True
@@ -16,14 +18,20 @@ async def main(page: ft.Page):
     gpu_text = "Loading..."
     ram_text = "Loading..."
     disk_text = "Loading..."
+    fps = "Loading..."
 
     def create_stat_card(title, value, icon, color, width):
         return ft.Container(
-            content=ft.Column( [
-                ft.Icon(icon, size=30, color=color),
-                ft.Text(title, size=16),
-                ft.Text(value, size=20, weight="bold"),
-            ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER, scroll=ft.ScrollMode.AUTO),
+            content=ft.Column(
+                [
+                    ft.Icon(icon, size=30, color=color),
+                    ft.Text(title, size=16),
+                    ft.Text(value, size=20, weight="bold"),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                scroll=ft.ScrollMode.AUTO,
+            ),
             bgcolor=ft.Colors.WHITE10,
             border_radius=25,
             padding=ft.padding.all(20),
@@ -34,10 +42,22 @@ async def main(page: ft.Page):
 
     stat_grid = ft.Row(
         controls=[
-            create_stat_card("CPU", cpu_text, ft.Icons.MEMORY, ft.Colors.BLUE, stat_box_width),
-            create_stat_card("GPU", gpu_text, ft.Icons.SIXTY_FPS_SELECT, ft.Colors.GREEN, stat_box_width),
-            create_stat_card("RAM", ram_text, ft.Icons.MEMORY, ft.Colors.ORANGE, stat_box_width),
-            create_stat_card("Disks", disk_text, ft.Icons.FOLDER, ft.Colors.PURPLE, stat_box_width),
+            create_stat_card(
+                "CPU", cpu_text, ft.Icons.MEMORY, ft.Colors.BLUE, stat_box_width
+            ),
+            create_stat_card(
+                "GPU",
+                gpu_text,
+                ft.Icons.SIXTY_FPS_SELECT,
+                ft.Colors.GREEN,
+                stat_box_width,
+            ),
+            create_stat_card(
+                "RAM", ram_text, ft.Icons.MEMORY, ft.Colors.ORANGE, stat_box_width
+            ),
+            create_stat_card(
+                "Disks", disk_text, ft.Icons.FOLDER, ft.Colors.PURPLE, stat_box_width
+            ),
         ],
         wrap=True,
         spacing=20,
@@ -52,7 +72,8 @@ async def main(page: ft.Page):
         controls=[
             stat_grid,
         ],
-    visible=False,)
+        visible=False,
+    )
 
     connection_view = ft.Column(
         scroll=ft.ScrollMode.AUTO,
@@ -60,45 +81,58 @@ async def main(page: ft.Page):
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         controls=[
             ft.Text("Enter Server IP to Connect", size=20),
-            ip := ft.TextField(label="Server IP", width=300, on_submit=lambda e: setattr(ip, 'value', e.control.value)),
+            ip := ft.TextField(
+                label="Server IP",
+                width=300,
+                on_submit=lambda e: setattr(ip, "value", e.control.value),
+            ),
         ],
-    visible=True,)
+        visible=True,
+    )
 
     benchmark_view = ft.Column(
         scroll=ft.ScrollMode.AUTO,
         alignment=ft.MainAxisAlignment.CENTER,
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         controls=[
-            ft.Text("Benchmark View Coming Soon!", size=20),
+            app_name := ft.TextField(
+                label="Game Name",
+                width=300,
+                on_submit=lambda e: setattr(app_name, "value", e.control.value),
+            ),
+            fps := ft.Text(fps),
         ],
-    visible=False,)
+        visible=False,
+    )
 
     views = [dashboard_view, connection_view, benchmark_view]
 
-    on_nav_change = lambda e: [
-        setattr(connection_view, 'visible', e.control.selected_index == 0),
-        setattr(dashboard_view, 'visible', e.control.selected_index == 1),
-        setattr(benchmark_view, 'visible', e.control.selected_index == 2),
-        page.update(),
-    ]
+    def on_nav_change(e):
+        setattr(connection_view, "visible", e.control.selected_index == 0)
+        setattr(dashboard_view, "visible", e.control.selected_index == 1)
+        setattr(benchmark_view, "visible", e.control.selected_index == 2)
+        page.update()
 
     page.navigation_bar = ft.NavigationBar(
-        destinations=[  
-            ft.NavigationBarDestination(icon=ft.Icons.WIFI, label="Connect"),     
-            ft.NavigationBarDestination(icon=ft.Icons.DASHBOARD, label="Dashboard"),      
-            ft.NavigationBarDestination(icon=ft.Icons.INSERT_CHART, label="Benchmark"), 
+        destinations=[
+            ft.NavigationBarDestination(icon=ft.Icons.WIFI, label="Connect"),
+            ft.NavigationBarDestination(icon=ft.Icons.DASHBOARD, label="Dashboard"),
+            ft.NavigationBarDestination(icon=ft.Icons.INSERT_CHART, label="Benchmark"),
         ],
-        on_change=on_nav_change
+        on_change=on_nav_change,
     )
 
     page.add(
-        ft.Container(
-            content=ft.Column(views, scroll=ft.ScrollMode.AUTO, expand=True),
-            padding=20,
+        ft.SafeArea(
+            ft.Container(
+                alignment=ft.alignment.center,
+                content=ft.Column(views, scroll=ft.ScrollMode.AUTO, expand=True),
+                padding=20,
+                expand=True,
+            ),
             expand=True,
-        )
+        ),
     )
-
 
     while True:
         if ip:
@@ -112,18 +146,29 @@ async def main(page: ft.Page):
                         cpu_text = f"{stats['cpu']['name']} \n Usage | {stats['cpu']['usage']}% \n Temperature | {stats['cpu']['temperature']}°C \n Clock Speed | {stats['cpu']['clock_speed']} Mhz \n Voltage | {stats['cpu']['voltage']}V"
                         gpu_text = f"{stats['gpu']['name']} \n Usage | {stats['gpu']['usage']}%"
                         ram_text = f"RAM Usage | {stats['ram']['used']} / {stats['ram']['total']} GB"
-                        disk_text = "\n".join([
-                            f"Disk {disk['device']} - Used | {disk['used']} / {disk['total']} GB"
-                            for disk in stats["disks"]
-                        ])
+                        disk_text = "\n".join(
+                            [
+                                f"Disk {disk['device']} - Used | {disk['used']} / {disk['total']} GB"
+                                for disk in stats["disks"]
+                            ]
+                        )
                         stat_grid.controls[0].content.controls[2].value = cpu_text
                         stat_grid.controls[1].content.controls[2].value = gpu_text
                         stat_grid.controls[2].content.controls[2].value = ram_text
                         stat_grid.controls[3].content.controls[2].value = disk_text
+                        if stats["benchmarking"]["is_running"]:
+                            fps.value = stats["benchmarking"]["fps"]
+                        if not app_name:
+                            pass
+                        if app_name:
+                            print(f"Printing app, {app_name.value}")
+                            await websocket.send(f"app, {app_name.value}")
                         page.update()
-                        await asyncio.sleep(1)
             except Exception as e:
-                print(f"Error connecting to server: {e} using {uri} - Retrying in 3 seconds...")
+                print(
+                    f"Error connecting to server: {e} using {uri} - Retrying in 3 seconds..."
+                )
                 await asyncio.sleep(3)
+
 
 ft.app(main)
